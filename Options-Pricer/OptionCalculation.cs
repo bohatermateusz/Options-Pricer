3af1f77c82d8348b1476db2f38450f7314bdc665
bohatermateusz <bohatermateusz@gmail.com>
2020-06-23 22:44:19 +0200

    add UI commands and code refactoring

--- Options-Pricer/OptionCalculation.cs
+++ Options-Pricer/OptionCalculation.cs
@@ -13,48 +13,41 @@ namespace Options_Pricer
         double exponentalResultRiskFreeIntrestRateResult { get; set; }
         double normalDistributionResultOfD2 { get; set; }
         double normalDistributionResultOfD1 { get; set; }
-        public OptionCalculation(int _stockPrice, int _strikePrice, int _riskFreeIntrestRate, int _timeToMaturity, int _standardDeviation)
+        internal OptionCalculation(int _stockPrice, int _strikePrice, int _timeToMaturity, int _standardDeviation, int _riskFreeIntrestRate)
         {
             stockPrice = _stockPrice;
             strikePrice = _strikePrice;
             riskFreeIntrestRate = _riskFreeIntrestRate;
             timeToMaturity = _timeToMaturity;
             standardDeviation = _standardDeviation;
-        }
 
-        private double D_1()
+            Variable_Calculations();
+        }
+        private double D_1_Value()
         {
             var d1 = (Math.Log(stockPrice / strikePrice) + (riskFreeIntrestRate + (Math.Pow(standardDeviation, 2)) / 2) * timeToMaturity)/(standardDeviation*Math.Sqrt(timeToMaturity));
             return d1;
         }
-
-        private double D_2(double d1)
+        private double D_2_Value(double d1)
         {
             var d2 = d1 - standardDeviation * Math.Sqrt(timeToMaturity);
             return d2;
         }
-
-        private double CallOptionPremiums()
+        internal double CallOptionPremiums()
         {
-            normalDistributionResultOfD1 = Normal.CDF(1, 0, D_1());
-
-            D_1();
-
-            exponentalResultRiskFreeIntrestRateResult = Exponential.CDF(riskFreeIntrestRate, timeToMaturity);
-            normalDistributionResultOfD2 = Normal.CDF(1, 0, D_2(D_1()));
-
-            var callOptionPremiumsResult = standardDeviation * normalDistributionResultOfD1 - strikePrice * exponentalResultRiskFreeIntrestRateResult * normalDistributionResultOfD2;
-            
+            var callOptionPremiumsResult = standardDeviation * normalDistributionResultOfD1 - strikePrice * exponentalResultRiskFreeIntrestRateResult * normalDistributionResultOfD2;   
             return callOptionPremiumsResult;
         }
-
-        private double OptionPrice()
+        internal double OptionPrice()
         {
             var optionPriceResult = strikePrice * exponentalResultRiskFreeIntrestRateResult * normalDistributionResultOfD2 - standardDeviation* normalDistributionResultOfD1;
             return optionPriceResult;
         }
-
-
-
+        private void Variable_Calculations()
+        {
+            normalDistributionResultOfD1 = Normal.CDF(1, 0, D_1_Value());
+            exponentalResultRiskFreeIntrestRateResult = Exponential.CDF(riskFreeIntrestRate, timeToMaturity);
+            normalDistributionResultOfD2 = Normal.CDF(1, 0, D_2_Value(D_1_Value()));
+        }
     }
 }
--- Options-Pricer/Program.cs
+++ Options-Pricer/Program.cs
@@ -6,7 +6,23 @@ namespace Options_Pricer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Please provide basic data for Option Calculus");
+            Console.WriteLine("Please provide Stock price:");
+            var s = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Strike price:");
+            var k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Time to maturity:");
+            var t = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Standard deviation of underlying stock:");
+            var sd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Risk-free interest rate:");
+            var r = int.Parse(Console.ReadLine());
+            
+            OptionCalculation instance = new OptionCalculation(s,k,t,sd,r);
+            
+            Console.WriteLine(instance.CallOptionPremiums());
+            Console.WriteLine(instance.OptionPrice());
         }
     }
 }
