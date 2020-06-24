821e81f5be240e985c18ab4fc029cbf86241bda6
bohatermateusz <bohatermateusz@gmail.com>
2020-06-24 15:32:16 +0200

    Code refactore

--- Options-Pricer/OptionCalculation.cs
+++ Options-Pricer/OptionCalculation.cs
@@ -23,31 +23,21 @@ namespace Options_Pricer
 
             Variable_Calculations();
         }
-        internal double CallOptionPremiums()
+        internal double CallOption()
         {
             var callOptionPremiumsResult = stockPrice * Normal.CDF(0, 1, d1) - strikePrice * exponentalResultRiskFreeIntrestRateResult * Normal.CDF(0, 1, d2);   
             return callOptionPremiumsResult;
         }
-        internal double OptionPrice()
+        internal double PutOption()
         {
             var optionPriceResult = strikePrice * exponentalResultRiskFreeIntrestRateResult * Normal.CDF(0, 1, - d2) - stockPrice* Normal.CDF(0, 1, - d1);
             return optionPriceResult;
         }
         private void Variable_Calculations()
         {
-            var log = (Math.Log(stockPrice / strikePrice));
-            var pow = (Math.Pow(standardDeviation, 2));
-            var sqrt = (Math.Sqrt(timeToMaturity));
-
             d1 = (Math.Log(stockPrice / strikePrice) + (riskFreeIntrestRate + (Math.Pow(standardDeviation, 2)) / 2) * timeToMaturity) / (standardDeviation * Math.Sqrt(timeToMaturity));
             d2 = d1 - standardDeviation * Math.Sqrt(timeToMaturity);
             exponentalResultRiskFreeIntrestRateResult = Exponential.PDF(1, timeToMaturity * riskFreeIntrestRate);
-        }
-
-        internal double NormalDistributionResult(double x)
-        {
-            var result = Normal.CDF(1, 0, x);
-            return result;
         }
     }
 }
--- Options-Pricer/Program.cs
+++ Options-Pricer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Options_Pricer
 {
@@ -6,25 +7,27 @@ namespace Options_Pricer
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello!");
-            //Console.WriteLine("Please provide basic data for Option Calculus");
-            //Console.WriteLine("Please provide Stock price:");
-            //var s = double.Parse(Console.ReadLine());
-            //Console.WriteLine("Please provide Strike price:");
-            //var k = double.Parse(Console.ReadLine());
-            //Console.WriteLine("Please provide Time to maturity:");
-            //var t = double.Parse(Console.ReadLine());
-            //Console.WriteLine("Please provide Standard deviation of underlying stock:");
-            //var sd = double.Parse(Console.ReadLine());
-            //Console.WriteLine("Please provide Risk-free interest rate:");
-            //var r = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Please provide basic data for Option Calculus");
+            Console.WriteLine("Please provide Stock price:");
+            var s = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Strike price:");
+            var k = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Time to maturity:");
+            var t = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Standard deviation of underlying stock:");
+            var sd = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please provide Risk-free interest rate:");
+            var r = double.Parse(Console.ReadLine());
 
-            OptionCalculation instance = new OptionCalculation(50, 55, 1, 0.2, 0.09);
-
-            //OptionCalculation instance = new OptionCalculation(s,k,t,sd,r);
+            OptionCalculation instance = new OptionCalculation(s,k,t,sd,r);
             
-            Console.WriteLine(instance.CallOptionPremiums());
-            Console.WriteLine(instance.OptionPrice());
+            Console.WriteLine("Your Put Option Value is:");
+            Console.WriteLine(instance.PutOption());
+
+            Console.WriteLine("Your Call Option Value is:");
+            Console.WriteLine(instance.CallOption());
+            Console.ReadLine();
         }
     }
 }
